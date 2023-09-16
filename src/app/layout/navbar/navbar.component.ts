27cd33b3360667a3823a6f7d2374ccb80089bd15
Darrell Couch <darrellneilcouch@gmail.com>
2023-09-16 17:14:01 -0500

    completed desktop site

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  public navClass = 'navbar';
+  public headingClass = 'navbar__heading';
+  public headingPClass = "navbar__heading--primary";
+  public headingSClass = "navbar__heading--secondary";
+  public menuClass = "navbar__menu"
+  public isAtTop = true;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 200) {
+      console.log(number)
+      this.isAtTop = false;
+      this.navClass = 'navbar--min';
+      this.headingClass = 'navbar__heading--min';
+      this.headingPClass = "navbar__heading--primary-min";
+      this.headingSClass = "navbar__heading--secondary-min";
+      this.menuClass = "navbar__menu--min";
+    } else {
+      this.navClass = 'navbar';
+      this.headingClass = 'navbar__heading';
+      this.headingPClass = "navbar__heading--primary";
+      this.headingSClass = "navbar__heading--secondary";
+      this.menuClass = "navbar__menu";
+      this.isAtTop = true;
+    }
+  }
 
 }
