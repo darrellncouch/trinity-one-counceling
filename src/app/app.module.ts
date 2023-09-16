32322103317e9d235c2727f323c2c05ddf0fced6
Darrell Couch <darrellneilcouch@gmail.com>
2023-09-15 23:36:09 -0500

    added hero and practice sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PracticeComponent } from './pages/practice/practice.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PracticeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
