f819231ad99d9380dd8d3640183955eec7cd2de3
Darrell Couch <darrellneilcouch@gmail.com>
2024-05-06 18:27:31 -0500

    moved content to config file

---
+++ src/core/config/assets.config.ts
@@ -0,0 +1,26 @@
+export const Assets = {
+  basePath: "../../assets/",
+  images: [
+    { id: 1, fileName: "1642297138554.jpg", altText: "Kino Counseling Logo" },
+    { id: 2, fileName: "1678117596747.jpg", altText: "Calvery Healing Center Logo" },
+    { id: 3, fileName: "iStock-500891249.webp", altText: "Tree overhanging a sidewalk path through grass" },
+    { id: 4, fileName: "iStock-503916737.webp", altText: "Woman basking in the sun" },
+    { id: 5, fileName: "iStock-1093110112.webp", altText: "Wood bridge over still water and through nature" }
+  ],
+  logos: {
+    noText: [
+      { size: "fav", fileName: "toc-fav.png" },
+      { size: "sm", fileName: "toc-no-text-sm.png" },
+      { size: "md", fileName: "toc-no-text-md.png" },
+      { size: "lg", fileName: "toc-no-text-lg.png" },
+      { size: "xl", fileName: "toc-no-text.png" },
+
+    ],
+    full: [
+      { size: "xs", fileName: "toc-full-xs.png" },
+      { size: "sm", fileName: "toc-full-sm.png" },
+      { size: "md", fileName: "toc-full-md.png" },
+      { size: "lg", fileName: "toc-full-lg.png" },
+    ]
+  }
+}
--- src/core/config/site.config.ts
+++ src/core/config/site.config.ts
@@ -1,4 +1,5 @@
 import { PhoneUtility } from "../utilities/phone.utility";
+import { Assets } from "./assets.config";
 
 export const siteConfig = {
   companyName: "Trinity-One Counseling",
@@ -16,7 +17,59 @@ export const siteConfig = {
     email: "kimkino@kniocounseling.com",
     phone: "4806508883",
     phoneDisplay: () => PhoneUtility.displayFormat(siteConfig.supervisorInfo.phone)
+  },
+  siteInfo: {
+    practice: {
+      header: "My Practice",
+      content: [
+        {
+          id: 1,
+          text: "Each of us has a certain perspective or predisposition as to how we handle the stress and anxiety of life. How we process the events and circumstances of our lives determines how mentally healthy we are. As we find ourselves affected negatively by circumstances, we may struggle to know how to manage and direct this part of ourselves so that it does not negatively impact our lives, but strengthens us. Life brings all sorts of situations our way that we may not know how to process in a healthy manner. This can lead to anger, anxiety, stress, or depression. Even trauma and loss are mostly out of our control but we need to process those things in a healthy way. Sometimes life can begin to feel unmanageable or maybe just uncomfortable and we may need help working through the stages in our life. It takes someone willing to connect with and listen and then help guide us through our own understanding and knowledge of mental health and healing. This is where I enjoy coming alongside my clients helping them to obtain the tools and wisdom to create simplicity and peace in their lives and to help heal their pain and suffering."
+        }
+      ]
+    },
+    treatment: {
+      header: "Treatment Preferences",
+      content: [
+        {
+          id: 1,
+          text: "I work together with my clients to administer treatment by using counseling modalities such as Mindfulness-Based Cognitive Therapy (MBCT), Person-Centered Therapy, Strength-Based Therapy, Eye Movement Desensitization Reprocessing (EMDR) and The Three Principles-Based techniques. Resilience is our common response to difficult life issues such as stress, anxiety, depression, trauma and loss and through resilience we can create sustainable change. I desire to support my clients through these evidence-based techniques to obtain this sustaining change for complete mental health.",
+        }
+      ]
+    },
+    about: {
+      header: "About Me",
+      content: [
+        {
+          id: 1,
+          text: "I am an Arizona native growing up in a family of eleven, eight siblings, me, and my mother and father. Raised in a Catholic community and grade school, this was the foundation that gave me a sense of love, belonging and the desire to be present for others. In my early teenage years my parents divorced and this affected me in ways I never knew until I was in my early 20s. At the time, I did not deal with my circumstances in a healthy way but instead tried to ignore the pain, only to have it surface later in much more painful ways to myself and others. This gave me the desire to learn more about human behaviors to help myself and others. I followed that desire by obtaining my Master’s Degree in Professional Counseling and further with a Post-Master’s Certificate in Trauma-Informed Care, both at Grand Canyon University. I have four years of experience in trauma-informed care and 20 years of experience managing a prominent mediation firm in Phoenix."
+        },
+        {
+          id: 2,
+          text: "Personally, I enjoy spending time with God and my adult children and grandchildren. I love hiking, bicycling and reading. I value my relationships because they have been key in showing me how to love myself and others. By putting “the other’s” interests ahead of our own we create a win-win experience which brings immense joy to each day. "
+        }
+      ]
+    },
+    professional: {
+      header: "Professional History",
+      content: [
+        {
+          id: 1,
+          logo: Assets.images[Assets.images.map(x => x.id).indexOf(2)],
+          titel: "Inpatient Therapist",
+          company: "Calvery Healing Center",
+          startDate: "Mar 2018",
+          endDate: "Present"
+        },
+        {
+          id: 2,
+          logo: Assets.images[Assets.images.map(x => x.id).indexOf(1)],
+          titel: "Licensed Associate Counselor",
+          company: "Kino Counseling Center",
+          startDate: "Apr 2023",
+          endDate: "Present"
+        }
+      ]
+    }
   }
-
-
 }
